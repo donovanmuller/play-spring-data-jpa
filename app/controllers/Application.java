6657a94a3b6c834cdc1e794b4e2d71eef7fd942b
Donovan Muller <donovan.muller@gmail.com>
2014-06-26 12:51:57 +0200

    Arg, whitespace.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,8 +27,7 @@ public class Application extends Controller {
         this.personRepository = personRepository;
     }
 
-    @Transactional
-    // <-- Let Spring manage our transaction boundaries. See @EnableTransactionManagement on SpringDataJpaConfiguration. You could add this annotation at class level as well, which means all controller actions will either participate in or start new transactions.
+    @Transactional // <-- Let Spring manage our transaction boundaries. See @EnableTransactionManagement on SpringDataJpaConfiguration. You could add this annotation at class level as well, which means all controller actions will either participate in or start new transactions.
     public Result index() {
 
         final Person person = new Person();
@@ -42,11 +41,9 @@ public class Application extends Controller {
         // Here the children collection for the savedPerson works, because we just added it before.
         final Person savedPerson = personRepository.save(person);
 
-
         // For retrievedPerson the children collection is not initialized because of a LIE.
         final Person retrievedPerson = personRepository.findOne(savedPerson.id);
         Logger.info("Got person, no LIE's: {} {}", retrievedPerson.firstname, retrievedPerson.surname);
-
 
         return ok(views.html.index.render("Found id: " + retrievedPerson.id + " of person/people with " + retrievedPerson.children.size() + " children"));
     }
